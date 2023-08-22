505781b336bbcfe25edd82f367510e976e34b4e2
github-username <etwa@ngs.ru>
2023-08-22 12:23:10 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,8 +38,31 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let result = "";
+    for (let i = 0, j = 10; i < expr.length; i = i + 10, j = j + 10) {
+        let num = "";
+        num = expr.slice(i, j);
+        let code = "";
+        if (num === "**********") {
+            code = code + " ";
+        } else {
+            for (let x = 0, y = 2; x < 10; x = x + 2, y = y + 2) {
+                if(num.slice(x, y) === "10") {
+                    code = code + ".";
+                }
+                else if(num.slice(x, y) === "11") {
+                    code = code + "-";
+                }
+            }  
+        }
+        if (code === " ") {
+            result = result + " ";
+        } else {
+            result = result + MORSE_TABLE[code]
+        }
+    }
+    return result
+    }
 
 module.exports = {
     decode
